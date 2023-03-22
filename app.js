418a90644ff4b744a77ed52cfa1281e284f7c8e2
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-03-22 16:58:29 -0300

    Make function that plays the game
    Invoking the playRound function 5 times the game begins asking
    the player to pick one value, then the computer picks a random value too
    and the scores increment depending the choices. When the player or
    the computer scores get to 3 the game finish and announce a winner.

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('hello world!');
-
 function getComputerChoice () {
     let number = Math.random() * 100;
     if (number <= 33) {
@@ -10,25 +8,101 @@ function getComputerChoice () {
         return 'Scissors'
     }
 }
-
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        return 'You Win! Rock beats Scissors'
+        return 'player-win'
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        return 'You Lose! Scissors beats Paper'
+        return 'computer-win'
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
-        return 'A Draw! Play Again'
+        return 'draw'
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return 'You Lose! Paper beats Rock'
+        return 'computer-win'
     } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
-        return 'A Draw! Play Again'
+        return 'draw'
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        return 'You Win! Scissors beats Paper'
+        return 'player-win'
     } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-        return 'A Draw! Play Again'
+        return 'draw'
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return 'You Win! Paper beats Rock'
+        return 'player-win'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'computer-win'
     } else {
-        return 'You Lose! Rock beats Scissors'
+        return 'ERROR'
     }
 }
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    let result1;
+    result1 = playRound(playerSelection = prompt('Pick', '...'), computerSelection = getComputerChoice());
+    if (result1 === 'player-win') {
+        playerScore ++;
+    } else if (result1 === 'computer-win') {
+        computerScore ++;
+    } else if (result1 === 'draw') {
+        playerScore ++;
+        computerScore ++;
+    } else {result1 = 'ERROR'}
+    console.log(`computer pick: ${computerSelection}`, result1, playerScore, computerScore)
+    let result2;
+    result2 = playRound(playerSelection = prompt('Pick', '...'), computerSelection = getComputerChoice());
+    if (result2 === 'player-win') {
+        playerScore ++;
+    } else if (result2 === 'computer-win') {
+        computerScore ++;
+    } else if (result2 === 'draw') {
+        playerScore ++;
+        computerScore ++;
+    } else {result2 = 'ERROR'}
+    console.log(`computer pick: ${computerSelection}`, result2, playerScore, computerScore)
+    let result3;
+    result3 = playRound(playerSelection = prompt('Pick', '...'), computerSelection = getComputerChoice());
+    if (result3 === 'player-win') {
+        playerScore ++;
+    } else if (result3 === 'computer-win') {
+        computerScore ++;
+    } else if (result3 === 'draw') {
+        playerScore ++;
+        computerScore ++;
+    } else {result3 = 'ERROR'}
+    console.log(`computer pick: ${computerSelection}`, result3, playerScore, computerScore)
+     if (playerScore >= 3) {
+        console.log('player wins')
+    } else if (computerScore >= 3) {
+        console.log('computer wins')
+    } else {
+    let result4;
+    result4 = playRound(playerSelection = prompt('Pick', '...'), computerSelection = getComputerChoice());
+    if (result4 === 'player-win') {
+        playerScore ++;
+    } else if (result4 === 'computer-win') {
+        computerScore ++;
+    } else if (result4 === 'draw') {
+        playerScore ++;
+        computerScore ++;
+    } else {result4 = 'ERROR'}
+    console.log(`computer pick: ${computerSelection}`, result4, playerScore, computerScore)
+    } 
+    if (playerScore >= 3) {
+    console.log('player wins')
+    } else if (computerScore >= 3) {
+    console.log('computer wins')
+    } else {
+    let result5;
+    result5 = playRound(playerSelection = prompt('Pick', '...'), computerSelection = getComputerChoice());
+    if (result5 === 'player-win') {
+        playerScore ++;
+    } else if (result5 === 'computer-win') {
+        playerScore ++;
+        computerScore ++;
+    } else if (result5 === 'draw') {
+        playerScore ++;
+        computerScore ++;
+    } else {result5 = 'ERROR'}
+    console.log(`computer pick: ${computerSelection}`, result5, playerScore, computerScore)
+    } if (playerScore >= 3) {
+    console.log('player wins')
+    } else if (computerScore >= 3) {
+    console.log('computer wins')
+    }}
