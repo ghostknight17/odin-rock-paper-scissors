4803accdeff3f9629eaccfc7280d81955d92b60e
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-03-22 12:16:33 -0300

    Create a function that returns the computer choice
    Using a random generated value for a number we get the computer to
    pick between 'Rock', 'Paper' and 'Scissors'.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('hello world!');
+function getComputerChoice () {
+    let number = Math.random() * 100;
+    if (number <= 33) {
+        return 'Rock'
+    } else if (number <= 66) {
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
+}
