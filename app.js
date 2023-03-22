25612a8088f7d0e1ad2bf541a094f25a1bb428b5
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-03-22 13:50:42 -0300

    Make function that plays a round of the game
    The functions returns the result of the round taking into account
    the parameters 'playerSelection' (choice made by user) and
    'computerSelection' which is gonna use the getComputerChoice
    function to pick a value.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('hello world!');
+
 function getComputerChoice () {
     let number = Math.random() * 100;
     if (number <= 33) {
@@ -7,5 +8,27 @@ function getComputerChoice () {
         return 'Paper'
     } else {
         return 'Scissors'
+    }
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You Win! Rock beats Scissors'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return 'A Draw! Play Again'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You Lose! Paper beats Rock'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        return 'A Draw! Play Again'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You Win! Scissors beats Paper'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return 'A Draw! Play Again'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You Win! Paper beats Rock'
+    } else {
+        return 'You Lose! Rock beats Scissors'
     }
 }
