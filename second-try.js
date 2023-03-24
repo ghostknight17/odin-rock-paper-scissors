8c2d73e16df41139463547d6d946418036c3baf5
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-03-24 00:46:12 -0300

    Complete rework the JavaScript file
    With this rework code is a little more clean and readable now,
    also the games finish correctly and scores go back to 0 after the
    player or the computer wins.

---
+++ second-try.js
@@ -0,0 +1,84 @@
+function getComputerChoice() {
+  let number = Math.random() * 100;
+  if (number <= 33) {
+    return 'Rock';
+  } else if (number <= 66) {
+    return 'Paper';
+  } else {
+    return 'Scissors';
+  }
+}
+
+function playRound(computerSelection, playerSelection) {
+  if (computerSelection === playerSelection) {
+    return 'draw';
+  } else if (
+    (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+    (computerSelection === 'Paper' && playerSelection === 'Rock') ||
+    (computerSelection === 'Scissors' && playerSelection === 'Paper')
+  ) {
+    return 'computer-wins';
+  } else if (
+    (computerSelection === 'Rock' && playerSelection === 'Paper') ||
+    (computerSelection === 'Paper' && playerSelection === 'Scissors') ||
+    (computerSelection === 'Scissors' && playerSelection === 'Rock')
+  ) {
+    return 'player-wins';
+  } else {
+    return 'ERROR, pick only between "Rock", "Paper" and "Scissors"';
+  }
+}
+
+function playGame() {
+  let roundResult = playRound(
+    (computerSelection = getComputerChoice()),
+    (playerSelection = prompt('Pick "Rock", "Paper" or "Scissors"', '...'))
+  );
+  if (roundResult === 'draw') {
+    playerScore + 0;
+    computerScore + 0;
+    console.log(
+      `A draw! Computer picked ${computerSelection}, 
+				Your score is: ${playerScore}, 
+				computer score is: ${computerScore}`
+    );
+    if (playerScore < 5 && computerScore < 5) {
+      playGame();
+    } else {
+      console.log('We have a winner!');
+    }
+  } else if (roundResult === 'computer-wins') {
+    playerScore + 0;
+    computerScore++;
+    console.log(
+      `You lose this one! Computer picked ${computerSelection}, 
+				Your score is: ${playerScore}, 
+				computer score is: ${computerScore}`
+    );
+    if (playerScore < 5 && computerScore < 5) {
+      playGame();
+    } else {
+      console.log('We have a winner!');
+    }
+  } else if (roundResult === 'player-wins') {
+    playerScore++;
+    computerScore + 0;
+    console.log(
+      `Good work! Computer picked ${computerSelection}, 
+				Your score is: ${playerScore}, 
+				computer score is: ${computerScore}`
+    );
+    if (playerScore < 5 && computerScore < 5) {
+      playGame();
+    } else {
+      playerScore = 0;
+      computerScore = 0;
+      console.log('We have a winner!');
+    }
+  } else {
+    console.log(roundResult);
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
